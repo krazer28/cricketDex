79ca21fdec934ecc5f7c441f07c6ab7eec35d76e
krazer28 <induacharya03@gmail.com>
2026-06-19 20:33:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-const CACHE_NAME = "cricketDex-v1.1";
-
 const CACHE_NAME = "cricketDex-v1.2";
 
 const filesToCache = [
@@ -14,35 +12,50 @@ const filesToCache = [
     "./manifest.json"
 ];
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+// Install
+self.addEventListener("install", event => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(filesToCache))
+    );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
+// Activate
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
 
-  self.clients.claim();
+    self.clients.claim();
+});
+
+// Network First Strategy
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
 });
